8618d844907e1f55827cec973d9690050836e360
evillanueeva <49422358+evillanueeva@users.noreply.github.com>
2019-04-11 11:28:37 -0300

    Add files via upload

---
+++ 00-cppHelloWorld/AED.cpp
@@ -0,0 +1,15 @@
+/*	Hola Mundo
+	Programa inicial para probar el compilador.
+	Emanuel Villanueva - 159583-0
+	11/04/2019
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	std::cin.get();
+	return 0;
+}
+
